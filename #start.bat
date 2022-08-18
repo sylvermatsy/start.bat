89649fda8305ed419076661a446f7f71a1293f5c
SylverMATSY <103262166+sylvermatsy@users.noreply.github.com>
2022-08-18 03:30:30 +0200

    Add files via upload

---
+++ #start.bat
@@ -0,0 +1,61 @@
+:menu
+@echo off & color 0f & cls & title start.bat
+mode 60,25
+color 03
+echo.
+echo        [1] Lancer Serveur
+echo.
+echo        [2] Supprimer cache puis Lancer le Serveur
+echo.
+echo        [3] Aide (Mon Discord de Developpement)
+echo.
+echo        [4] Fermer ce programme.
+echo.
+set /p answer="Quelle option choisissez-vous ? > "
+if /i {%ANSWER%}=={1} (goto :1)
+if /i {%ANSWER%}=={2} (goto :2)
+if /i {%ANSWER%}=={3} (goto :3)
+if /i {%ANSWER%}=={4} (goto :4)
+goto :menu
+exit
+
+:1
+color 03
+echo.
+echo Lancement du serveur en cours...
+ping localhost -n 2 >nul
+echo.
+cd /d "C:\EMPLACEMENT"
+"C:\EMPLACEMENT\FXServer.exe" +exec server.cfg
+rem Changez les 2 chemins d'accès ci-dessus
+pause
+exit
+
+:2
+color 03
+echo.
+echo Suppression du cache en cours...
+RMDIR /s /q "C:\EMPLACEMENT\cache\"
+rem Changez le chemin d'accès ci-dessus
+echo.
+ping localhost -n 2 >nul
+echo Dossier cache supprime ! Demarage du Server ...
+echo.
+ping localhost -n 2 >nul
+echo.
+cd /d "C:\EMPLACEMENT"
+"C:\EMPLACEMENT\FXServer.exe" +exec server.cfg
+rem Changez les 2 chemins d'accès ci-dessus
+
+:3
+echo.
+color 03
+echo Redirection vers le discord de T-Dev !
+echo Redirection en cours ...
+start https://discord.gg/HKk8MQMaMW
+
+:4
+echo.
+echo Fermeture du programme en cours...
+ping localhost -n 3 >nul
+exit
